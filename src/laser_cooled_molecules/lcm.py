51875ac2736221c46b883d95e21848b23fea7313
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-02-19 08:46:36 -0800

    Color horizontally

--- src/laser_cooled_molecules/lcm.py
+++ src/laser_cooled_molecules/lcm.py
@@ -2,46 +2,42 @@ import argparse
 from dataclasses import dataclass
 from typing import Iterator
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatch
 import numpy as np
 import matplotlib.ticker as mticker
 import matplotlib
 from datetime import date
 
-alt_colors = [
-    '#0F9D58', # Green
-    '#F4B400', # Yellow
-    '#4285F4', # Blue
-    '#DB4437', # Red
-]
 matplotlib.rcParams['font.family'] = 'Roboto'
-matplotlib.rcParams['axes.prop_cycle'] = matplotlib.cycler(color=alt_colors)
+
+natoms_to_color = {
+        'diatomics': '#F4B400', # Yellow
+        'triatomics': '#0F9D58', # Green
+        '>3 atoms': '#4285F4', # Blue
+}
 
 @dataclass
 class Molecule:
     """ Name and year of first laser cooling of a molecule. """
     name: str
     date: date
+    natoms: int
 
-diatomics = [
-        Molecule(name='SrF', date=date(2010, 9, 19)),
-        Molecule(name='YO', date=date(2013, 4, 1)),
-        Molecule(name='CaF', date=date(2014, 5, 16)),
-		Molecule(name='YbF', date=date(2018, 3, 22)),
-		Molecule(name='BaH', date=date(2020, 8, 18)),
-		Molecule(name='CaH', date=date(2022, 8, 9)),
-		Molecule(name='BaF', date=date(2022, 3, 14)),
-		Molecule(name='CaD', date=date(2024, 8, 5)),
+molecules = [
+        Molecule(name='SrF', date=date(2010, 9, 19), natoms=2),
+        Molecule(name='YO',  date=date(2013, 4,  1), natoms=2),
+        Molecule(name='CaF', date=date(2014, 5, 16), natoms=2),
+		Molecule(name='YbF', date=date(2018, 3, 22), natoms=2),
+		Molecule(name='BaH', date=date(2020, 8, 18), natoms=2),
+		Molecule(name='CaH', date=date(2022, 8, 9), natoms=2),
+		Molecule(name='BaF', date=date(2022, 3, 14), natoms=2),
+		Molecule(name='CaD', date=date(2024, 8, 5), natoms=2),
+        Molecule(name='SrOH', date=date(2017, 4, 24), natoms=3),
+        Molecule(name='YbOH', date=date(2020, 2, 19), natoms=3),
+        Molecule(name='CaOH', date=date(2020, 3, 31), natoms=3),
+        Molecule(name='CaOCH$_3$', date=date(2020, 9, 11), natoms=6),
 ]
-
-triatomics = [
-    Molecule(name='SrOH', date=date(2017, 4, 24)),
-    Molecule(name='YbOH', date=date(2020, 2, 19)),
-    Molecule(name='CaOH', date=date(2020, 3, 31)),
-]
-
-polyatomics = [
-    Molecule(name='CaOCH$_3$', date=date(2020, 9, 11)),
-]
+molecules.sort(key=lambda molecule: molecule.date)
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -86,9 +82,7 @@ def accumulate_by_year(
 
 def main():
     args = get_args()
-    timeline = [
-        molecule.date.year for molecule in diatomics + triatomics + polyatomics
-    ]
+    timeline = [molecule.date.year for molecule in molecules]
 
     years = np.arange(
         start=min(timeline)-1,
@@ -104,92 +98,41 @@ def main():
     )
     ax = fig.subplots()
 
-    diatomics_by_year = accumulate_by_year(
+    total_by_year = accumulate_by_year(
         yit=iter(years),
-        mit=iter(diatomics),
+        mit=iter(molecules),
     )
-    triatomics_by_year = accumulate_by_year(
-        yit=iter(years),
-        mit=iter(triatomics),
-    )
-    polyatomics_by_year = accumulate_by_year(
-        yit=iter(years),
-        mit=iter(polyatomics),
-    )
-    di_and_tri_by_year = [
-        t + d for t,d in zip(triatomics_by_year, diatomics_by_year)
-    ]
-    all_by_year = [
-        dt + p for dt, p in zip(di_and_tri_by_year, polyatomics_by_year)
-    ]
     
-    ax.fill_between(
+    ax.step(
         x=years,
-        y1=all_by_year,
-        y2=di_and_tri_by_year,
-        step='post',
-        alpha=0.4,
-    )
-    ax.fill_between(
-        x=years,
-        y1=di_and_tri_by_year,
-        y2=diatomics_by_year,
-        step='post',
-        alpha=0.4,
-    )
-    ax.fill_between(
-        x=years,
-        y1=diatomics_by_year,
-        y2=[0] * len(years),
-        step='post',
-        alpha=0.4,
+        y=total_by_year,
+        where='post',
+        ls='-',
+        color='k',
+        lw=2,
     )
 
-    ax.step(
-        x=years,
-        y=all_by_year,
-        where='post',
-        label='>3 atoms',
-    )
-    ax.step(
-        x=years,
-        y=di_and_tri_by_year,
-        where='post',
-        label='triatomics',
-    )
-    ax.step(
-        x=years,
-        y=diatomics_by_year,
-        where='post',
-        label='diatomics',
-    )
-
-    for pos, molecule in enumerate(diatomics):
+    for pos, molecule in enumerate(molecules):
         ax.text(
-            x=2025,
+            x=2025.1,
             y=pos + 0.5,
             s=molecule.name,
             ha='left',
             va='center',
         )
-    for pos, molecule in enumerate(triatomics):
-        offset = len(diatomics)
-        ax.text(
-            x=2025,
-            y=offset + pos + 0.5,
-            s=molecule.name,
-            ha='left',
-            va='center',
+        if molecule.natoms == 2:
+            category = "diatomics"
+        elif molecule.natoms == 3:
+            category = "triatomics"
+        else:
+            category = ">3 atoms"
+        ax.fill_between(
+            x=[molecule.date.year, years[-1]],
+            y1=[pos]*2,
+            y2=[pos + 1]*2,
+            color=natoms_to_color[category],
         )
-    for pos, molecule in enumerate(polyatomics):
-        offset = len(diatomics) + len(triatomics)
-        ax.text(
-            x=2025,
-            y=offset + pos + 0.5,
-            s=molecule.name,
-            ha='left',
-            va='center',
-        )
+
 
     ax.xaxis.set_label_text('Year')
     ax.yaxis.set_label_text('# of laser-cooled molecules')
@@ -212,9 +155,17 @@ def main():
     )
 
     ax.set_xlim(2009, 2025)
-    ax.set_ylim(0, len(diatomics + triatomics + polyatomics)+0.2)
+    ax.set_ylim(0, len(molecules)+0.2)
 
-    plt.legend()
+    handles = list()
+    for category, color in natoms_to_color.items():
+        patch = mpatch.Patch(
+            color=color,
+            label=category,
+        )
+        handles.append(patch)
+    ax.legend(handles=handles)
+
     if args.save is True:
         fig.savefig('laser_cooled_molecules.pdf')
     else:
